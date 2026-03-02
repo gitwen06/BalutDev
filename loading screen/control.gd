5b7ca11e074848bbb2985b3f6f5acbe8e9f2009a
PrimozJay <johnmichaelgeneralao1@gmail.com>
2026-03-02 22:08:37 +0800

    UPT J

---
+++ loading screen/control.gd
@@ -0,0 +1,20 @@
+extends Control
+
+@onready var progress_bar = $ProgressBar
+
+var scene_path = "res://Your3DScene.tscn"
+var load_status
+var progress = []
+
+func _ready():
+	ResourceLoader.load_threaded_request(scene_path)
+
+func _process(delta):
+	load_status = ResourceLoader.load_threaded_get_status(scene_path, progress)
+	
+	if load_status == ResourceLoader.THREAD_LOAD_IN_PROGRESS:
+		progress_bar.value = progress[0] * 100
+	
+	elif load_status == ResourceLoader.THREAD_LOAD_LOADED:
+		var scene = ResourceLoader.load_threaded_get(scene_path)
+		get_tree().change_scene_to_packed(scene)
