f7ad8c6d60c87c07f57a9557c268e4f2b107202c
Trace019 <glowseperate2.0@gmail.com>
2026-03-02 21:49:50 +0800

    a

--- interaction/interaction_manager.gd
+++ interaction/interaction_manager.gd
@@ -21,6 +21,7 @@ func _process(delta):
 		label.text = baseText + activeAreas[0].action_name
 		label.global_position = activeAreas[0].global_positon
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
 		label.global_position.y -= 36
 		label.global_position.x -= label.size.x / 2
 		label.show()
@@ -28,11 +29,14 @@ func _process(delta):
 		label.hide()
 =======
 >>>>>>> Stashed changes
+=======
+>>>>>>> Stashed changes
 
 func sort_by_distance_to_player(area1, area2):
 	var area1_to_player = player.global_position.distamce_to(area1.global_position)
 	var area2_to_player = player.global_position.distamce_to(area2.global_position)
 	return area1_to_player > area2_to_player
+<<<<<<< Updated upstream
 <<<<<<< Updated upstream
 	
 func _input(event):
@@ -43,5 +47,7 @@ func _input(event):
 				
 			await activeAreas[0].interact.call()
 			canInteract = true
+=======
+>>>>>>> Stashed changes
 =======
 >>>>>>> Stashed changes
