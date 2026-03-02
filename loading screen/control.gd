dbac58c1a67f92fff31d0ee77f5e3d2963a23dd1
PrimozJay <johnmichaelgeneralao1@gmail.com>
2026-03-02 22:36:13 +0800

    UPT J

--- loading screen/control.gd
+++ loading screen/control.gd
@@ -1,7 +1,6 @@
 extends Control
 
 @onready var progress_bar = $ProgressBar
-
 var scene_path = "res://Main3DScene.tscn"
 var progress = []
 
@@ -10,10 +9,8 @@ func _ready():
 
 func _process(delta):
 	var status = ResourceLoader.load_threaded_get_status(scene_path, progress)
-
 	if status == ResourceLoader.THREAD_LOAD_IN_PROGRESS:
 		progress_bar.value = progress[0] * 100
-
 	elif status == ResourceLoader.THREAD_LOAD_LOADED:
 		var scene = ResourceLoader.load_threaded_get(scene_path)
 		get_tree().change_scene_to_packed(scene)
