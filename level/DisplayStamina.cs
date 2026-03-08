c28cd8c09c3003c5865d96659abd41f5bcb9db99
Trace019 <glowseperate2.0@gmail.com>
2026-03-08 20:24:51 +0800

    a
    a

--- level/DisplayStamina.cs
+++ level/DisplayStamina.cs
@@ -1,19 +1,13 @@
 using Godot;
 using System;
 
-public partial class DisplayStamina : Label
-{
+public partial class DisplayStamina : Label {
 	private Player player;
-
-	public override void _Ready()
-	{
+	public override void _Ready() {
 		player = GetNode<Player>("/root/playerGlobals");
 	}
-
-	public override void _Process(double delta)
-	{
-		if (player != null)
-		{
+	public override void _Process(double delta) {
+		if (player != null) {
 			Text = $"Stamina: {player.stamina}";
 		}
 	}
