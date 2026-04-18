7e1442acf8b0f2385b0b17c48ddd060a29c7d959
gitwen06 <shellwinescorial14@gmail.com>
2026-04-18 22:49:38 +0800

    UPD
    fixed player model, fixed stamina bar not woorking, fixed scaling

--- level/progress_bar.gd
+++ level/progress_bar.gd
@@ -4,10 +4,10 @@ var player
 
 func _ready() -> void:
 	player = get_node("/root/playerGlobals")
-	min_value = 10000
+	min_value = 1
 	max_value = player.maxStamina
 	value = player.stamina
 
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	if player:
 		value = player.stamina
